a23bc36c1983f794bdd69f8f6772c9ff16d11253
Shyam JVS <shyam123.jvs95@gmail.com>
2014-08-28 17:15:12 +0530

    Added sql script for creating cities table

---
+++ sql_scripts/create_cities_table.sql
@@ -0,0 +1,11 @@
+create table cities(
+    city varchar(20),
+    country varchar(30),
+    population integer);
+
+insert into cities values ('athens','greece',1368);
+insert into cities values ('bangkok','thailand',1178);
+insert into cities values ('barcelona','spain',1280);
+insert into cities values ('berlin','east_germany',3481);
+insert into cities values ('birmingham','united_kingdom',1112);
+

e9e9be14fbc045bcadf8a9cb8a455ebb65916555
Anurag Shirolkar <ashirolkar47@gmail.com>
2014-09-26 19:52:39 +0530

    rulebased conversion working - chunker_v2

--- python_code/chunker_v2.py
+++ python_code/chunker_v2.py
@@ -32,7 +32,7 @@ def get_child(self, node_val):
 nltk.Tree.get_child = get_child
 
 action = wordmap[select_part.get_child('VB').get_child('VERB')[0]]
-print action
+#print action
 
 fields = []
 temp = select_part
@@ -42,7 +42,54 @@ while(True):
         break
     fields.append(temp.get_child('FIELD').get_child('ATTRIB')[0])
 
-print ', '.join(fields)
+#print ', '.join(fields)
 
-print from_part
+look = from_part.get_child('LOOK')[0]
+tables = []
+while look != 'from':
+    from_part = from_part.get_child('FROM')
+    look = from_part.get_child('LOOK')[0]
+if look == 'from':
+    temp = from_part
+    while True:
+        temp = temp.get_child('INTO')
+        if temp == False:
+            break
+        tables.append(temp.get_child('TABLE_').get_child('TABLE')[0])
 
+#print ', '.join(tables)
+
+
+condmap = {'equal':'=', 'unequal':'!=', 'more':'>', 'less':'<', 'higher':'>', 'lower':'<', 'high':'>', 'low':'<', 'greater':'>', 'lesser':'<'}
+condmap_neg = {'equal':'!=', 'unequal':'=', 'more':'<=', 'less':'>=', 'higher':'<=', 'lower':'>=', 'high':'<=', 'low':'>=', 'greater':'<=', 'lesser':'>='}
+
+conds = []
+temp = where_part
+while True:
+    cc = temp.get_child('CC')
+    if cc != False:
+        cc = cc[0]
+        conds.append(cc)
+    temp = temp.get_child('CONDS')
+    if temp == False:
+        break
+    cond = temp.get_child('COND')
+    val1 = cond[0][0][0]
+    val2 = cond[2][0][0]
+    vbx = cond[1].get_child('VBX')
+    comp = cond[1].get_child('COMP')
+    if comp == False and vbx[0] == 'is':
+        cond = val1 + ' = ' + val2
+    else :
+        neg = comp.get_child('NEG')
+        if neg == False:
+            comp2 = comp.get_child('COMP2')[0]
+            cond = val1 + ' ' + condmap[comp2] + ' ' + val2
+        else :
+            comp2 = comp.get_child('COMP2')[0]
+            cond = val1 + ' ' + condmap_neg[comp2][0] + ' ' + val2
+    conds.append(cond)
+
+#print ' '.join(conds)
+
+print action + ' ' + ', '.join(fields) + ' from ' + ','.join(tables) + ' where ' + ' '.join(conds)
